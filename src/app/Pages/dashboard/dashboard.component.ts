0ea6f20d9f1bab027b3b37d7345f641252b7960a
Mohamed Hazem <mohazemwork@gmail.com>
2024-07-25 17:57:30 +0300

    header and night mode

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MyHeaderComponent } from '../../Utility/my-header/my-header.component';
 
 @Component({
   selector: 'dashboard-home',
   standalone: true,
-  imports: [],
+  imports: [MyHeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
